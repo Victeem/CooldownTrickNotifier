5b647d24fe45052beb646138a6f51fe2f93bcf27
Victim726 <captaincook726@gmail.com>
2026-02-24 18:37:30 -0500

    Forgot to rename mixin class

--- src/client/java/gay/plat/ctn/mixin/MinecraftMixin.java
+++ src/client/java/gay/plat/ctn/mixin/MinecraftMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
-public abstract class PiercingWeaponComponentMixin {
+public abstract class MinecraftMixin {
     @Shadow
     @Nullable
     public LocalPlayer player;
